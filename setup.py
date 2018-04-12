30c2ad42d48641bde2113bbb6f77ae873354c7c0
David Caron <dcaron05@gmail.com>
2018-04-11 21:47:13 -0400

    Version bump (not an official version)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ CLASSIFIERS = [
 
 setup(
     name = 'CppHeaderParser',
-    version = '2.7.3',
+    version = '2.7.3.1',
     author = 'Jashua Cloutier',
     author_email = 'jashuac@bellsouth.net',
     url = 'http://senexcanis.com/open-source/cppheaderparser/',
